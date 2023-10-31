cf8d17fcd8666a1b984b9f534b5076485f1bc088
KangbingZhao <447953883@qq.com>
2023-10-31 22:06:09 +0800

    import netty

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 dependencies {
+	implementation("io.netty:netty-all:4.1.100.Final")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
